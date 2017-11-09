3c97a5d92ed8cb8db7744be769ee836371ab1621
GlennCavarle <glenn.cavarle@gmail.com>
2017-11-09 10:49:42 +0100

    add option 'names-only' in console command 'blast:doctrine:list-tables'

--- src/Blast/Bundle/ResourceBundle/Command/ListDoctrineTablesCommand.php
+++ src/Blast/Bundle/ResourceBundle/Command/ListDoctrineTablesCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Blast Project package.
  *
@@ -9,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
-
 namespace Blast\Bundle\ResourceBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -19,6 +17,7 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Description of ListDoctrineTablesCommand.
@@ -27,18 +26,22 @@ use Symfony\Component\Console\Input\InputArgument;
  */
 class ListDoctrineTablesCommand extends ContainerAwareCommand
 {
+
     protected function configure()
     {
         $this
-                ->setName('blast:doctrine:list-tables')
-                ->setDescription('List doctrine tables')
-                ->addArgument('preg_filter', InputArgument::OPTIONAL,
-                        'table name filter used by preg_match', '.*');
+            ->setName('blast:doctrine:list-tables')
+            ->setDescription('List doctrine tables')
+            ->addOption('names-only', null, InputOption::VALUE_NONE, 'show table names only')
+            ->addArgument('preg_filter', InputArgument::OPTIONAL,
+                'table name filter used by preg_match', '.*');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $filter = $input->getArgument('preg_filter');
+        $namesOnly = $input->hasOption('names-only');
+
         $command = $this->getApplication()->find('doctrine:schema:create');
         $arguments = array('--dump-sql' => true);
         $cmdInput = new ArrayInput($arguments);
@@ -47,12 +50,12 @@ class ListDoctrineTablesCommand extends ContainerAwareCommand
 
         $content = $cmdOutput->fetch();
         preg_match_all(
-                '#create\s*table\s* (\w+)\s*\((.*)\) #i', $content, $matches);
+            '#create\s*table\s* (\w+)\s*\((.*)\) #i', $content, $matches);
         $tableNames = $matches[1];
         $columns = $matches[2];
         $tables = [];
 
-        for ($i = 0; $i < count($columns); ++$i) {
+        for ( $i = 0; $i < count($columns); ++$i ) {
             $cols = explode(',', $columns[$i]);
 
             $cols = array_map(function ($c) {
@@ -63,16 +66,19 @@ class ListDoctrineTablesCommand extends ContainerAwareCommand
 
         asort($tables);
 
-        foreach ($tables as $tableName => $cols) {
-            if (!preg_match('/.*' . $filter . '.*/', $tableName)) {
+        foreach ( $tables as $tableName => $cols ) {
+            if ( !preg_match('/.*' . $filter . '.*/', $tableName) ) {
                 continue;
             }
             $table = new Table($output);
             $table->setHeaders([$tableName]);
-
-            foreach ($cols as $colName) {
-                $table->addRow([$colName]);
+            
+            if ( false === $namesOnly ) {
+                foreach ( $cols as $colName ) {
+                    $table->addRow([$colName]);
+                }
             }
+            
             $table->render();
         }
     }
