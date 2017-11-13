8c9f54c58afaab31329ab2ad86485dfcfa905082
PapsOu <vivien.frasca@libre-informatique.fr>
2017-11-13 12:58:35 +0100

    Clean Ecommerce outer extensions

--- src/AppBundle/Entity/SilMediaBundle/File.php
+++ src/AppBundle/Entity/SilMediaBundle/File.php
@@ -15,4 +15,54 @@ use Sil\Bundle\MediaBundle\Entity\File as BaseFile;
 
 class File extends BaseFile
 {
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var mixed
+     */
+    private $owner;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    public function setOwner($owner)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
 }
