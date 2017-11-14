05e12cf4ec5d5e99559141c7e5f654d04e0d8017
François GERBES <francois.gerbes@libre-informatique.fr>
2017-11-14 09:58:43 +0100

    ci install doc + auto script for other ci script

--- bin/ci-scripts/install_docs.sh
+++ bin/ci-scripts/install_docs.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 set -ev
 
-pip install -r src/Resources/doc/requirements.txt --user
+pip install -r src/AppBundle/Resources/doc/requirements.txt --user
+
+bin/ci-scripts/do_it_for_bundle.sh install docs
