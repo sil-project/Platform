a0320a508d45de39ccf8ee4de06fc083a3f643c3
François GERBES <francois.gerbes@libre-informatique.fr>
2017-11-14 11:31:47 +0100

    [ci] Create travis database

--- bin/ci-scripts/create_travis_database_test.sh
+++ bin/ci-scripts/create_travis_database_test.sh
@@ -4,6 +4,14 @@ set -ev
 # Database creation
 
 ###
+### mysql
+###
+
+# (mysql service is started by default by travis for each build instance
+# (mysql travis user is created by travis for each build instance
+# mysql -u travis -e 'CREATE DATABASE travis;' -v
+
+###
 ### postgresql
 ###
 
@@ -21,6 +29,14 @@ psql -c 'ALTER DATABASE sil OWNER TO sil_user' -U postgres
 
 
 psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d sil
+
+
+# travis user already exist on travis
+psql -c 'CREATE DATABASE travis;' -U postgres
+psql -c 'ALTER DATABASE travis OWNER TO travis' -U postgres
+
+
+psql -U postgres -c "CREATE EXTENSION 'uuid-ossp';" -U postgres -d travis
 
 ###
 ###
