c48d2e689b847ed7b88676bb70d0fe813b4bf726
François GERBES <francois.gerbes@libre-informatique.fr>
2017-11-14 11:25:15 +0100

    [CI] create sil database

--- bin/ci-scripts/create_travis_database_test.sh
+++ bin/ci-scripts/create_travis_database_test.sh
@@ -13,14 +13,14 @@ set -ev
 # or here :  sudo /etc/init.d/postgresql start
 
 # (we try to create a travis user)
-psql -c "CREATE USER lisem_user WITH PASSWORD 'lisem';" -U postgres
-psql -c 'ALTER ROLE lisem_user WITH CREATEDB;' -U postgres
+psql -c "CREATE USER sil_user WITH PASSWORD 'sil';" -U postgres
+psql -c 'ALTER ROLE sil_user WITH CREATEDB;' -U postgres
 
-psql -c 'CREATE DATABASE lisem;' -U postgres
-psql -c 'ALTER DATABASE lisem OWNER TO lisem_user' -U postgres
+psql -c 'CREATE DATABASE sil;' -U postgres
+psql -c 'ALTER DATABASE sil OWNER TO sil_user' -U postgres
 
 
-psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d lisem
+psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d sil
 
 ###
 ###
