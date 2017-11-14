213bf9a28b84204e40bd99f894af3c7f9b285563
François GERBES <francois.gerbes@libre-informatique.fr>
2017-11-14 11:36:55 +0100

    [ci] syntax bugfix

--- bin/ci-scripts/create_travis_database_test.sh
+++ bin/ci-scripts/create_travis_database_test.sh
@@ -30,13 +30,12 @@ psql -c 'ALTER DATABASE sil OWNER TO sil_user' -U postgres
 
 psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d sil
 
-
+# create it for bundle phpunit test
 # travis user already exist on travis
 psql -c 'CREATE DATABASE travis;' -U postgres
 psql -c 'ALTER DATABASE travis OWNER TO travis' -U postgres
 
-
-psql -U postgres -c "CREATE EXTENSION 'uuid-ossp';" -U postgres -d travis
+psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d travis
 
 ###
 ###
