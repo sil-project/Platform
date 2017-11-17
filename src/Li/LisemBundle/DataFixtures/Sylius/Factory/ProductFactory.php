8c6ea659558869f3644fd8bda9117720026a98bd
PapsOu <vivien.frasca@libre-informatique.fr>
2017-11-17 10:00:05 +0100

    Fixed mapping association by setting join table name

--- src/Li/LisemBundle/DataFixtures/Sylius/Factory/ProductFactory.php
+++ src/Li/LisemBundle/DataFixtures/Sylius/Factory/ProductFactory.php
@@ -1,18 +1,17 @@
 <?php
 
 /*
- * This file is part of the Lisem Project.
  *
  * Copyright (C) 2015-2017 Libre Informatique
  *
- * This file is licenced under the GNU GPL v3.
+ * This file is licenced under the GNU LGPL v3.
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
 namespace LisemBundle\DataFixtures\Sylius\Factory;
 
-use Sil\Bundle\EcommerceBundle\Entity\Product;
+use LisemBundle\Entity\SilEcommerceBundle\Product;
 use Sil\Bundle\VarietyBundle\Entity\Variety;
 use Sylius\Component\Product\Factory\ProductFactoryInterface;
 use Sylius\Component\Product\Repository\ProductOptionRepositoryInterface;
