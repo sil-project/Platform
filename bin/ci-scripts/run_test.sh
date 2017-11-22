34f64c177cab5a50d217995b1afd85483e054a92
François GERBES <francois.gerbes@libre-informatique.fr>
2017-11-22 10:50:23 +0100

    [CI] Update Run Test to Check for Output failed file

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -1,9 +1,39 @@
 #!/usr/bin/env sh
 
+#!/usr/bin/env sh
+
 vendor/phpunit/phpunit/phpunit -v -c phpunit.xml.dist
 #--coverage-clover build/logs/clover.xml
 
-vendor/bin/codecept run Lisem -d --steps --fail-fast --no-interaction --no-exit --env=firefox -g login
-
 
 #bin/ci-scripts/do_it_for_bundle.sh run test
+
+
+OUTPUTDIR=tests/_output
+
+# clean output
+rm -rf $OUTPUTDIR/*.png
+rm -rf $OUTPUTDIR/*.html
+
+CODECEPTCMD="bin/codecept run Lisem -d --steps --fail-fast --no-interaction --no-exit"
+
+
+CODECEPTGROUP=$@
+if [ $# -eq 0 ]
+then
+   CODECEPTGROUP="login " #menu user crm variety ecommerce" # all"
+fi
+
+
+
+for i in $CODECEPTGROUP
+do
+    $CODECEPTCMD -g $i --env=firefox
+    #$CODECEPTCMD -g $i --env=chrome
+done
+
+
+# check output
+NBFAIL=$(find $OUTPUTDIR |grep fail|wc -w)
+exit $NBFAIL;
+
