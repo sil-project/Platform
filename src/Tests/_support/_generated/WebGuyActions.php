4cacd625dc0e8e723efe27fde012b9323e668c87
PapsOu <vivien.frasca@libre-informatique.fr>
2017-11-22 09:33:43 +0100

    Stylefix

--- src/Tests/_support/_generated/WebGuyActions.php
+++ src/Tests/_support/_generated/WebGuyActions.php
@@ -7,7 +7,7 @@
  * This file is licenced under the GNU LGPL v3.
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
- */ 
+ */
 
 namespace _generated;
 
