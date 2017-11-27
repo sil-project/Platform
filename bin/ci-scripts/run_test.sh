8500aeb20c3866a07ba89ed9209c18e3d038840c
PapsOu <vivien.frasca@libre-informatique.fr>
2017-11-27 11:54:18 +0100

    [CI] Add all tests for travis

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -25,7 +25,7 @@ CODECEPTCMD="bin/codecept run Lisem -d --steps --fail-fast --no-interaction "
 CODECEPTGROUP=$@
 if [ $# -eq 0 ]
 then
-   CODECEPTGROUP="login menu crm" #menu user crm variety ecommerce" # all"
+   CODECEPTGROUP="all" #login menu user crm variety seedbatch ecommerce" # all"
 fi
 
 
