b1f0ba615989016b7f28a2bea7c3afc7e10aa0c0
PapsOu <vivien.frasca@libre-informatique.fr>
2017-12-05 17:21:53 +0100

    Remove SeedBatchBundle and VarietyBundle (will be included in lisem)

--- src/Sil/Bundle/SeedBatchBundle/bin/ci-scripts/run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-vendor/phpunit/phpunit/phpunit -v -c phpunit.xml.dist --coverage-clover build/logs/clover.xml
-
--- src/Sil/Bundle/VarietyBundle/bin/ci-scripts/run_docs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-cd Resources/doc
-sphinx-build -b html -d _build/doctrees . _build/html
