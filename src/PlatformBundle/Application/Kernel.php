0f98b738e3927a344194695d550ae4cea734fbc1
François GERBES <francois.gerbes@libre-informatique.fr>
2017-12-06 11:56:10 +0100

    Enable WebServer for test env

--- src/PlatformBundle/Application/Kernel.php
+++ src/PlatformBundle/Application/Kernel.php
@@ -158,11 +158,8 @@ class Kernel extends BaseKernel
             $bundles[] = new \Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-
-            if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-                $bundles[] = new \Symfony\Bundle\WebServerBundle\WebServerBundle();
-            }
+            $bundles[] = new \Symfony\Bundle\WebServerBundle\WebServerBundle();
+            $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
