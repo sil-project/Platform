77bdae89515b0bd6fc3afc4273362b5e05ff2008
PapsOu <vivien.frasca@libre-informatique.fr>
2017-12-11 17:16:57 +0100

    [CI] Disable bundle requirements

--- bin/ci-scripts/install_docs.sh
+++ bin/ci-scripts/install_docs.sh
@@ -3,4 +3,4 @@ set -ev
 
 pip install -r src/PlatformBundle/Resources/doc/requirements.txt --user
 
-bin/ci-scripts/do_it_for_bundle.sh install docs
+# bin/ci-scripts/do_it_for_bundle.sh install docs
--- bin/ci-scripts/run_docs.sh
+++ bin/ci-scripts/run_docs.sh
@@ -4,4 +4,4 @@ cd src/PlatformBundle/Resources/doc
 sphinx-build -b html -d _build/doctrees . _build/html
 cd -
 
-bin/ci-scripts/do_it_for_bundle.sh run docs
+# bin/ci-scripts/do_it_for_bundle.sh run docs
