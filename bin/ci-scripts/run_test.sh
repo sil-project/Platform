e5cc74aaf924b7743b4e444780039a520b31275d
PapsOu <vivien.frasca@libre-informatique.fr>
2017-12-14 09:57:39 +0100

    [CI] Stylefix

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -5,7 +5,7 @@ if [ -f .env ]
 then
     source .env
 else
-    echo "Please run this script from project root, and check .env file as it is mandatory"    
+    echo "Please run this script from project root, and check .env file as it is mandatory"
     echo "If it is missing a quick solution is :"
     echo "ln -s .env.travis .env"
     exit 42
@@ -23,27 +23,27 @@ fi
 if [ -n "$CODECEPTCMD" ]
 then
     OUTPUTDIR=$CODECEPTOUTPUT
-    
+
     # clean output
     rm -rf $OUTPUTDIR/*.png
     rm -rf $OUTPUTDIR/*.html
-    
-    
+
+
     CODECEPTGROUP=$@
     if [ $# -eq 0 ]
     then
         CODECEPTGROUP="login menu" #login menu user crm variety seedbatch ecommerce" # all"
     fi
-    
-    
-    
+
+
+
     for i in $CODECEPTGROUP
     do
         $CODECEPTCMD -g $i --env=$CODECEPTENV
     done
-    
-    
+
+
     # check output
     #NBFAIL=$(find $OUTPUTDIR |grep fail|wc -w)
     #exit $NBFAIL;
-fi 
+fi
