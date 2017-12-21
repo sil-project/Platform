764780e828f53ab1bd5a4eba859f47db15aafbb5
François GERBES <francois.gerbes@libre-informatique.fr>
2017-12-21 16:06:21 +0100

    [CI] Before install update for coveralls

--- src/Blast/Bundle/SearchBundle/bin/ci-scripts/before_install_test.sh
+++ src/Blast/Bundle/SearchBundle/bin/ci-scripts/before_install_test.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 set -ev
 
-# check syntax
-find . -name '*.php' -exec php -l {} \;
-
 # Ugly hack
 echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 
@@ -11,5 +8,8 @@ composer self-update --stable
 
 composer global require --no-interaction cedx/coveralls
 
+mkdir -p ${HOME}/bin
+
+ln -s ${HOME}/.config/composer/vendor/bin/coveralls ${HOME}/bin/coveralls
 
 
