1e4979073c45e6e86be3ee6e3b10299e02a459be
François GERBES <francois.gerbes@libre-informatique.fr>
2018-01-02 17:49:38 +0100

    [CI] Drop Database !

--- bin/ci-scripts/create_travis_database_test.sh
+++ bin/ci-scripts/create_travis_database_test.sh
@@ -23,6 +23,11 @@ set -v
 #  - postgresql
 # or here :  sudo /etc/init.d/postgresql start
 
+
+psql -w -c "DROP DATABASE IF EXISTS test_sil_db;" -U postgres
+psql -w -c "DROP ROLE IF EXISTS test_sil_user;" -U postgres
+
+
 # (we try to create a travis user)
 psql -w -c "CREATE USER test_sil_user WITH PASSWORD 'test_sil_password';" -U postgres
 psql -w -c 'ALTER ROLE test_sil_user WITH CREATEDB;' -U postgres
