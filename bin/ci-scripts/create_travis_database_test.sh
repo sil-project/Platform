956c5309f1b94bc65751f021b3dc98e9a463b39c
François GERBES <francois.gerbes@libre-informatique.fr>
2018-01-02 15:41:37 +0100

    [CI] psql do not ask for password

--- bin/ci-scripts/create_travis_database_test.sh
+++ bin/ci-scripts/create_travis_database_test.sh
@@ -21,21 +21,21 @@ set -v
 # or here :  sudo /etc/init.d/postgresql start
 
 # (we try to create a travis user)
-psql -c "CREATE USER test_sil_user WITH PASSWORD 'test_sil_password';" -U postgres
-psql -c 'ALTER ROLE test_sil_user WITH CREATEDB;' -U postgres
+psql -w -c "CREATE USER test_sil_user WITH PASSWORD 'test_sil_password';" -U postgres
+psql -w -c 'ALTER ROLE test_sil_user WITH CREATEDB;' -U postgres
 
-psql -c 'CREATE DATABASE test_sil_db;' -U postgres
-psql -c 'ALTER DATABASE test_sil_db OWNER TO test_sil_user' -U postgres
+psql -w -c 'CREATE DATABASE test_sil_db;' -U postgres
+psql -w -c 'ALTER DATABASE test_sil_db OWNER TO test_sil_user' -U postgres
 
 
-psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d test_sil_db
+psql -w -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d test_sil_db
 
 # create it for bundle phpunit test
 # travis user already exist on travis
-#psql -c 'CREATE DATABASE travis;' -U postgres
-#psql -c 'ALTER DATABASE travis OWNER TO travis' -U postgres
+#psql -w -c 'CREATE DATABASE travis;' -U postgres
+#psql -w -c 'ALTER DATABASE travis OWNER TO travis' -U postgres
 
-#psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d travis
+#psql -w -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d travis
 
 ###
 ###
