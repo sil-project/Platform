72c366d1071332d16b26496b6c01efdc3db49528
PapsOu <vivien.frasca@libre-informatique.fr>
2018-01-05 10:15:01 +0100

    [Ecommerce] Fix order note inline edit popup

--- src/Sil/Bundle/EcommerceBundle/Resources/public/js/notes_inline_edit.js
+++ src/Sil/Bundle/EcommerceBundle/Resources/public/js/notes_inline_edit.js
@@ -1,30 +1,26 @@
-
 $('.notes-inline-edit').each(
     function(key, element) {
         var notes = $('.notes-inline-edit');
         var data = notes.data();
-    
-        notes.editable(
-            {
-                pk: 1,
-                type: 'textarea',
-                url: data.url,
-                mode: 'popup',
-                savenochange: false,
-                emptytext: '',
-                display: false,
-                params: {
-                    id: data.objectId,
-                    field: data.field
-                },
-                success: function(value) {
-                    if(value) {
-                        $(notes).siblings('.notes-value').text(value);
-                    }
+
+        notes.editable({
+            value: $(element).parent().find('.notes-value').html(),
+            pk: 1,
+            type: 'textarea',
+            url: data.url,
+            mode: 'popup',
+            savenochange: false,
+            emptytext: '',
+            display: false,
+            params: {
+                id: data.objectId,
+                field: data.field
+            },
+            success: function(value) {
+                if (value) {
+                    $(notes).siblings('.notes-value').text(value);
                 }
             }
-        );
+        });
     }
 );
-
-
