53667683703898de9fa833b0556f2582caf6da55
François GERBES <francois.gerbes@libre-informatique.fr>
2018-01-15 16:20:39 +0100

    [CI] Disable kill of selenium server

--- bin/ci-scripts/launch_selenium_test.sh
+++ bin/ci-scripts/launch_selenium_test.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/env bash
 set -v
 
-
-# stop already running selenium server
-for i in $(lsof -t -i :4444)
-do
-    kill $i
-done
-
-
+#export PATH=$PATH:./bin
+ 
 # start fake x
 /sbin/start-stop-daemon --start --pidfile /tmp/xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1680x1050x16
 export DISPLAY=:99
 
+# stop already running selenium server
+#for i in $(lsof -t -i :4444)
+#do
+#    kill $i
+#done
 
-#export PATH=$PATH:./bin
 sel_start_date=$(date)
-#bin/selenium-server-standalone -debug -enablePassThrough false > selenium.log 2>&1  &
-java -jar ${HOME}/bin/selenium-server-standalone.jar -debug -enablePassThrough false  > selenium.log 2>&1  &
 
-set +e
+lsof -t -i :4444
+if [ $? -ne 0 ]
+then
+    
+    #bin/selenium-server-standalone -debug -enablePassThrough false > selenium.log 2>&1  &
+    java -jar ${HOME}/bin/selenium-server-standalone.jar -debug -enablePassThrough false  > selenium.log 2>&1  &
+    
+fi
+
 while [ $(netstat -an | grep LISTEN | grep 4444| wc -l) -eq 0 ]
 do
     echo $(date) " wait for selenium start... (since " $sel_start_date ")";
     ps -eaf | grep selenium;
     netstat -an | grep LISTEN | grep 4444;
+    lsof -t -i :4444
     sleep 10;
 done
 
+
 echo $(date) " it look like selenium is started (waiting since " $sel_start_date ")";
+    
 
