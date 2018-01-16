cf57d2b57782c2a604db5b9f8f5d425a7fe54b03
François GERBES <francois.gerbes@libre-informatique.fr>
2018-01-16 16:08:21 +0100

    [CI] disable forced version of composer

--- bin/ci-scripts/before_install_lint.sh
+++ bin/ci-scripts/before_install_lint.sh
@@ -3,4 +3,5 @@ set -ev
 
 mkdir --parents "${HOME}/bin"
 
-composer self-update 1.5.6 --no-progress --stable
+#composer self-update 1.5.6 --no-progress --stable
+composer self-update --no-progress --stable
