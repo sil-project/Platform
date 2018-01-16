a526adeaf1a7df8acdc98d3b6d2345ab61402074
François GERBES <francois.gerbes@libre-informatique.fr>
2018-01-16 15:29:13 +0100

    [CI] disable forced version of composer

--- bin/ci-scripts/before_install_test.sh
+++ bin/ci-scripts/before_install_test.sh
@@ -16,7 +16,8 @@ then
     #sudo apt-get install chromium-chromedriver
 fi
    
-composer self-update 1.5.6 --no-progress --stable
+#composer self-update 1.5.6 --no-progress --stable
+composer self-update --no-progress --stable
 #composer clear-cache
 
 if [ ! -x ${HOME}/bin/chromedriver ]
