539df307d129a77faa113913abdbc67fed64ff83
François GERBES <francois.gerbes@libre-informatique.fr>
2018-01-26 15:21:35 +0100

    [CI] Try to reset elk index before populate

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -7,7 +7,7 @@ if [ -f .env ]
 then
     source .env
 else
-    echo "Please run this script from project root, and check .env file as it is mandatory"    
+    echo "Please run this script from project root, and check .env file as it is mandatory"
     echo "If it is missing a quick solution is :"
     echo "ln -s .env.travis .env"
     exit 42
@@ -35,7 +35,8 @@ bin/console doctrine:schema:create --no-interaction --em=session --env=$SERVEREN
 #bin/console doctrine:schema:update --force --no-interaction --env=$SERVERENV
 #bin/console doctrine:schema:validate --no-interaction --env=$SERVERENV
 
-#bin/console fos:elastica:populate --no-reset --no-interaction --env=$SERVERENV
+bin/console fos:elastica:reset --no-interaction --env=$SERVERENV
+bin/console fos:elastica:populate --no-interaction --env=$SERVERENV
 
 # Not needed as it is launch in composer install
 #bin/console blast:patchs:apply --no-interaction --env=$SERVERENV
