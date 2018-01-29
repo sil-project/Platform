7b88cf40d1bfa40f0c44bc42b2223d2096fb605c
GlennCavarle <glenn.cavarle@gmail.com>
2018-01-29 12:15:31 +0100

    [ci] update doc install en run

--- bin/ci-scripts/install_docs.sh
+++ bin/ci-scripts/install_docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-pip install -r src/doc/fr/requirements.txt --user
+pip install -r doc/fr/requirements.txt --user
 
 # bin/ci-scripts/do_it_for_bundle.sh install docs
--- bin/ci-scripts/run_docs.sh
+++ bin/ci-scripts/run_docs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cd src/doc/fr
+cd doc/fr
 sphinx-build -b html -d _build/doctrees . _build/html
 cd -
-cd src/doc/en
+cd doc/en
 sphinx-build -b html -d _build/doctrees . _build/html
 cd -
 # bin/ci-scripts/do_it_for_bundle.sh run docs
