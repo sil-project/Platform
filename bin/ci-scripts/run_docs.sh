ced626eae888732c30c6b3189a0bd33d258f6fa7
GlennCavarle <glenn.cavarle@gmail.com>
2018-01-29 12:05:05 +0100

    [ci] update doc install en run

--- bin/ci-scripts/run_docs.sh
+++ bin/ci-scripts/run_docs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-cd src/PlatformBundle/Resources/doc
+cd src/doc/fr
 sphinx-build -b html -d _build/doctrees . _build/html
 cd -
-
+cd src/doc/en
+sphinx-build -b html -d _build/doctrees . _build/html
+cd -
 # bin/ci-scripts/do_it_for_bundle.sh run docs
