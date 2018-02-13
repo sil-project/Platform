74a9725944174f294e2fa4c49f96345e41f3a1fa
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-13 12:03:03 +0100

    [CI] Rename namespace for test order

--- src/Sil/Component/Order/Tests/Unit/OrderProcessorTest.php
+++ src/Sil/Component/Order/Tests/Unit/OrderProcessorTest.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sil\Component\Product\Tests\Unit;
+namespace Sil\Component\Order\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Sil\Component\Order\Tests\Unit\Fixtures\Fixtures;
