c530f82a8ba34fbd380174b1b1240928b7a00635
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-13 16:32:01 +0100

    [CI] update path for doc generation

--- bin/ci-scripts/run_docs.sh
+++ bin/ci-scripts/run_docs.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -ev
 
-cd doc/fr
-sphinx-build -b html -d _build/doctrees . _build/html
-cd -
-cd doc/en
-sphinx-build -b html -d _build/doctrees . _build/html
+#-d _build/doctrees
+
+
+sphinx-build -b html doc/fr build/sphinx
+
+#cd doc/en
+#sphinx-build -b html -d _build/doctrees . _build/html
 cd -
 # bin/ci-scripts/do_it_for_bundle.sh run docs
