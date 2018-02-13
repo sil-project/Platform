2b697d7818b7b595a5e914dfae41cf9639978ac2
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-13 12:11:52 +0100

    [CI] Rename namespace for test order

--- src/Sil/Component/Order/Tests/Unit/AdjustmentTest.php
+++ src/Sil/Component/Order/Tests/Unit/AdjustmentTest.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sil\Component\Product\Tests\Unit;
+namespace Sil\Component\Order\Tests\Unit;
 
 use DomainException;
 use PHPUnit\Framework\TestCase;
