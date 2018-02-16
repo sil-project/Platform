9aa1dddf6d56dfdf6141f58aa61b7e530f8c9e29
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-16 18:09:16 +0100

    [CI] Only one commit message for subrepo push

--- bin/git-scripts/push-subrepo.sh
+++ bin/git-scripts/push-subrepo.sh
@@ -4,4 +4,18 @@ set -ex
 git checkout master
 git pull
 git config branch.master.rebase false
+
+save_ref=$(git rev-parse HEAD)
+
 git subrepo push --all -p '[CI]'
+
+git reset --soft $save_ref
+for i in $(find . -name '.gitrepo' -exec dirname {} \;)
+do
+    cd $i
+    git config --file=".gitrepo" subrepo.parent $save_ref
+    git add .gitrepo
+    cd -
+done
+
+git commit -m '[CI] Push All Subrepo'
