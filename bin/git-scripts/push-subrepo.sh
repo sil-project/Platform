95049cc884bbe0168636a126d08b40b02cb3e23a
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-19 10:32:28 +0100

    [CI] Check if file as been modified

--- bin/git-scripts/push-subrepo.sh
+++ bin/git-scripts/push-subrepo.sh
@@ -13,9 +13,16 @@ git reset --soft $save_ref
 for i in $(find . -name '.gitrepo' -exec dirname {} \;)
 do
     cd $i
-    git config --file=".gitrepo" subrepo.parent $save_ref
-    git add .gitrepo
+
+    # find if file have been modified as we don't want to update unpushed subrepo
+    gst=$(git status -s $i | grep 'M' | grep '.gitrepo' | wc -l)
+    if [ $gst -eq 1 ]
+       then
+           git config --file=".gitrepo" subrepo.parent $save_ref
+           git add .gitrepo
+    fi
     cd -
 done
+
 
 git commit -m '[CI] Push All Subrepo'
