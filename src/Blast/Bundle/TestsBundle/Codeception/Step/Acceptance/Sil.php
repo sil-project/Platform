1cb016360c38b30c27a4ed02c4ea36d64b7440b2
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-20 17:29:00 +0100

    [CI] disable useless check on Libre string

--- src/Blast/Bundle/TestsBundle/Codeception/Step/Acceptance/Sil.php
+++ src/Blast/Bundle/TestsBundle/Codeception/Step/Acceptance/Sil.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2015-2017 Libre Informatique
+ * Copyright (C) 2015-2018 Libre Informatique
  *
  * This file is licenced under the GNU LGPL v3.
  * For the full copyright and license information, please view the LICENSE.md
@@ -37,7 +37,7 @@ class Sil extends Common
         $this->fillField("//input[@id='_username']", $username);
         $this->fillField("//input[@id='_password']", $password);
         $this->click("//button[@type='submit']");
-        $this->waitForText('Libre', 30);
+
         // $this->amOnPage($this->getSilUrl() . '/dashboard'); // all steps should call amOnPage before doing any actions
         //$this->hideSymfonyToolBar(); //useless for test and may hide important element
     }
