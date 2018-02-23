f0e21bd7cbc4cc4ef72111442c5e7489ae10d8bb
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-23 17:39:20 +0100

    [CI] quick fix to test deployement on sandbox

--- bin/deploy-scripts/gen_archive.sh
+++ bin/deploy-scripts/gen_archive.sh
@@ -24,3 +24,20 @@ echo "Version=${Version}" >>  ${Pkgdir}/env.to.deploy
 echo "Filename=${Filename}" >> ${Pkgdir}/env.to.deploy
 
 sha256sum ${Pkgdir}/${Filename} > ${Pkgdir}/sha256.to.deploy
+
+
+if [ -f .env ]
+then
+    source .env
+else
+    echo "Please run this script from project root, and check .env file as it is mandatory"
+    echo "If it is missing a quick solution is :"
+    echo "ln -s .env.travis .env"
+    exit 42
+fi
+
+# temp for deployement
+# Todo : should handle database
+# Maybe nslookup ip
+
+psql -w -h ${DBHOST} -c  "update sil_ecommerce_channel set hostname='sandboxrd.libre-informatique.fr';" -U ${DBROOTUSER} -d ${DBAPPNAME}
