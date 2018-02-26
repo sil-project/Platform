fd49a781bf2bc1e3abf61be9e0ece8c61345e558
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-26 15:51:07 +0100

    [CI] Add git meta info in txt file

--- bin/ci-scripts/gen_archive.sh
+++ bin/ci-scripts/gen_archive.sh
@@ -6,6 +6,9 @@ Name=Platform
 Version=$(git describe --tags)
 Tag=$(git describe --tags --abbrev=0)
 Filename=${Name}_${Version}.tar.gz
+echo ${Version} > Version.txt
+echo ${Tag} > Tag.txt
+
 rm -f ${Filename}
 
 # gen archive --transform='s|\./|./'${Tag}'/|g'
