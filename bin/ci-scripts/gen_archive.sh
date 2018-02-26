1f53139358308b0fbaccb267f5421323a32e75c3
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-26 13:51:31 +0100

    [CI] Clean script

--- bin/ci-scripts/gen_archive.sh
+++ bin/ci-scripts/gen_archive.sh
@@ -6,21 +6,10 @@ Name=Platform
 Version=$(git describe --tags)
 Tag=$(git describe --tags --abbrev=0)
 Filename=${Name}_${Version}.tar.gz
-Pkgdir=./
 rm -f ${Filename}
 
 # gen archive --transform='s|\./|./'${Tag}'/|g'
 tar -czf ${Filename} ./*
-mkdir -p ${Pkgdir}
-mv ${Filename} ${Pkgdir}
 
-# gen deploy meta
-#rm -f ${Pkgdir}/env.to.deploy
-#touch ${Pkgdir}/env.to.deploy
 
-#echo "Name=${Name}" >> ${Pkgdir}/env.to.deploy
-#echo "Tag=${Tag}" >> ${Pkgdir}/env.to.deploy
-#echo "Version=${Version}" >>  ${Pkgdir}/env.to.deploy
-#echo "Filename=${Filename}" >> ${Pkgdir}/env.to.deploy
-
-sha256sum ${Pkgdir}/${Filename} > ${Pkgdir}/${Filename}.sha256
+sha256sum ${Filename} > ${Filename}.sha256
