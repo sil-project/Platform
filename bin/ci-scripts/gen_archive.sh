65cf83aaf3bd83891e33d82118241d0e67bfdf13
François GERBES <francois.gerbes@libre-informatique.fr>
2018-02-27 10:52:07 +0100

    [CI] Use only branch/tag as info

--- bin/ci-scripts/gen_archive.sh
+++ bin/ci-scripts/gen_archive.sh
@@ -3,17 +3,18 @@ set -ex
 
 Name=Platform
 # git symbolic-ref -q --short HEAD || git describe --tags --exact-match
-Version=$(git describe --tags)
-Tag=$(git describe --tags --abbrev=0)
-Branch=$(git name-rev  --name-only $(git rev-parse HEAD) | sed -e s/\\^.*//g | cut -f2 -d'/')
+#Version=$(git describe --tags)
+#Tag=$(git describe --tags --abbrev=0)
+Branch=$(git name-rev  --name-only $(git rev-parse HEAD) | sed -e s/\\^.*//g | cut -f-1 -d'/')
 
 # Clean current git dir
 git clean -df
 git checkout -- .
 
-Filename=${Name}_${Version}.tar.gz
-echo ${Version} > Version.txt
-echo ${Tag} > Tag.txt
+#Filename=${Name}_${Version}.tar.gz
+Filename=${Name}_${Branch}.tar.gz
+#echo ${Version} > Version.txt
+#echo ${Tag} > Tag.txt
 echo ${Branch} > Branch.txt
 
 rm -f ${Filename}
