741e07f5741dce3290131d333e262567133100fe
François GERBES <francois.gerbes@libre-informatique.fr>
2018-03-07 17:27:53 +0100

    [CI] Add table creation for test

--- src/Blast/Bundle/CsvImportBundle/bin/ci-scripts/create_table_test.sh
+++ src/Blast/Bundle/CsvImportBundle/bin/ci-scripts/create_table_test.sh
@@ -18,9 +18,80 @@ then
 fi
 
 psql -w -h ${DBHOST}  -U ${DBROOTUSER} -d ${DBAPPNAME} <<EOF
-SELECT 'Hello ${DBAPPUSER}'
+DROP TABLE IF EXISTS public.test_final_table;
+
+CREATE TABLE public.test_final_table
+(
+  id integer NOT NULL,
+  final_parent_id integer,
+  parent_id integer,
+  name character varying(255) DEFAULT NULL::character varying,
+  code character varying(15) DEFAULT NULL::character varying,
+  CONSTRAINT test_final_table_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.test_final_table
+  OWNER TO ${DBAPPUSER};
+EOF
+
+
+
+psql -w -h ${DBHOST}  -U ${DBROOTUSER} -d ${DBAPPNAME} <<EOF
+DROP TABLE IF EXISTS public.test_parent_table;
+
+CREATE TABLE public.test_parent_table
+(
+  id integer NOT NULL,
+  second_id integer,
+  parent_parent_id integer,
+  name character varying(255) DEFAULT NULL::character varying,
+  code character varying(15) DEFAULT NULL::character varying,
+  CONSTRAINT test_parent_table_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.test_parent_table
+  OWNER TO ${DBAPPUSER};
+
+EOF
+
+
+psql -w -h ${DBHOST}  -U ${DBROOTUSER} -d ${DBAPPNAME} <<EOF
+DROP TABLE IF EXISTS public.test_second_table;
+
+CREATE TABLE public.test_second_table
+(
+  id integer NOT NULL,
+  simple_id integer,
+  name character varying(255) DEFAULT NULL::character varying,
+  code character varying(15) DEFAULT NULL::character varying,
+  CONSTRAINT test_second_table_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.test_second_table
+  OWNER TO ${DBAPPUSER};
+
 EOF
 
 psql -w -h ${DBHOST}  -U ${DBROOTUSER} -d ${DBAPPNAME} <<EOF
-SELECT 'Hello ${DBAPPUSER}'
+DROP TABLE IF EXISTS public.test_simple_table;
+
+CREATE TABLE public.test_simple_table
+(
+  id integer NOT NULL,
+  name character varying(255) DEFAULT NULL::character varying,
+  code character varying(15) DEFAULT NULL::character varying,
+  CONSTRAINT test_simple_table_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.test_simple_table
+  OWNER TO ${DBAPPUSER};
+
 EOF
