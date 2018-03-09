5a79739e984f0331cad70c8cf83aed64a196034e
François GERBES <francois.gerbes@libre-informatique.fr>
2018-03-09 19:01:04 +0100

    [CI] Try etcd env on build

---
+++ bin/ci-scripts/set_env_with_etcd.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -ex
+
+export ETCDCTL_API=3
+
+# rand number to avoid build colision (same db used by two build)
+if [ ! -f shuf.nbr ]
+then
+    shuf -i 200-600 -n 1 > shuf.nbr
+fi
+
+#RND may contain branch with '-' or upper case char which may not work as database name for postgre
+prefix=$(echo $RND|sed -e s/-/_/g|tr '[:upper:]' '[:lower:]')$(echo -n $(cat shuf.nbr ))
+
+if [ -z "$ETCDHOST" ]
+then
+    ETCDHOST="etcd.host"
+fi
+
+if [ -z "$ETCDCTLCMD" ]
+then
+    #ETCDCTLCMD="docker exec $ETCDHOST etcdctl "
+    ETCDCTLCMD="etcdctl "
+fi
+
+# check
+$ETCDCTLCMD get --endpoints="http://${ETCDHOST}:2379" --prefix '/default'
+
+# get postgres default
+postgreshost=$($ETCDCTLCMD get /default/postgres/hostname --print-value-only --endpoints="http://${ETCDHOST}:2379")
+postgresuser=$($ETCDCTLCMD get /default/postgres/root/username --print-value-only --endpoints="http://${ETCDHOST}:2379")
+postgrespass=$($ETCDCTLCMD get /default/postgres/root/password --print-value-only --endpoints="http://${ETCDHOST}:2379")
+# TODO add a check default cnx with psql
+
+# get elastic default
+elastichost=$($ETCDCTLCMD get /default/elastic/hostname --print-value-only --endpoints="http://${ETCDHOST}:2379")
+
+
+# set postgres env
+$ETCDCTLCMD put /build/$prefix/postgres/hostname $postgreshost
+$ETCDCTLCMD put /build/$prefix/postgres/root/username $postgresuser
+$ETCDCTLCMD put /build/$prefix/postgres/root/password $postgrespass
+
+$ETCDCTLCMD put /build/$prefix/postgres/user/dbname sil_db_$prefix
+$ETCDCTLCMD put /build/$prefix/postgres/user/username sil_user_$prefix
+$ETCDCTLCMD put /build/$prefix/postgres/user/password sil_password_$prefix
+
+# set elastic env
+$ETCDCTLCMD put /build/$prefix/elastic/hostname $elastichost
+$ETCDCTLCMD put /build/$prefix/elastic/indexalias sil_$prefix
+
+# set symfony env
+$ETCDCTLCMD put /build/$prefix/symfony/env test # maybe put this in env variable (or not)
+$ETCDCTLCMD put /build/$prefix/symfony/addr '127.0.0.1:8042'
+
+
+
+$ETCDCTLCMD get --endpoints="http://${ETCDHOST}:2379" --prefix /build/$prefix
