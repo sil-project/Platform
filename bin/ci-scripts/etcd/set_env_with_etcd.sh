6f1bc28062a88fccebd5de974bff24ea9dfa5073
François GERBES <francois.gerbes@libre-informatique.fr>
2018-03-12 11:17:28 +0100

    [CI] Force endpoint on each command + add debug

--- bin/ci-scripts/etcd/set_env_with_etcd.sh
+++ bin/ci-scripts/etcd/set_env_with_etcd.sh
@@ -16,43 +16,44 @@ if [ -z "$ETCDHOST" ]
 then
     ETCDHOST="etcd.host"
 fi
+ETCDENDPOINT="--endpoints='http://${ETCDHOST}:2379'"
 
 if [ -z "$ETCDCTLCMD" ]
 then
     #ETCDCTLCMD="docker exec $ETCDHOST etcdctl "
-    ETCDCTLCMD="etcdctl "
+    ETCDCTLCMD="etcdctl"
 fi
 
 # check
-$ETCDCTLCMD get --endpoints="http://${ETCDHOST}:2379" --prefix '/default'
+$ETCDCTLCMD get  --prefix '/default'
 
 # get postgres default
-postgreshost=$($ETCDCTLCMD get /default/postgres/hostname --print-value-only --endpoints="http://${ETCDHOST}:2379")
-postgresuser=$($ETCDCTLCMD get /default/postgres/root/username --print-value-only --endpoints="http://${ETCDHOST}:2379")
-postgrespass=$($ETCDCTLCMD get /default/postgres/root/password --print-value-only --endpoints="http://${ETCDHOST}:2379")
+postgreshost=$($ETCDCTLCMD get /default/postgres/hostname --print-value-only $ETCDENDPOINT)
+postgresuser=$($ETCDCTLCMD get /default/postgres/root/username --print-value-only $ETCDENDPOINT)
+postgrespass=$($ETCDCTLCMD get /default/postgres/root/password --print-value-only $ETCDENDPOINT)
 # TODO add a check default cnx with psql
 
 # get elastic default
-elastichost=$($ETCDCTLCMD get /default/elastic/hostname --print-value-only --endpoints="http://${ETCDHOST}:2379")
+elastichost=$($ETCDCTLCMD get /default/elastic/hostname --print-value-only $ETCDENDPOINT)
 
 
 # set postgres env
-$ETCDCTLCMD put /build/$prefix/postgres/hostname $postgreshost
-$ETCDCTLCMD put /build/$prefix/postgres/root/username $postgresuser
-$ETCDCTLCMD put /build/$prefix/postgres/root/password $postgrespass
+$ETCDCTLCMD put /build/$prefix/postgres/hostname $postgreshost $ETCDENDPOINT
+$ETCDCTLCMD put /build/$prefix/postgres/root/username $postgresuser $ETCDENDPOINT
+$ETCDCTLCMD put /build/$prefix/postgres/root/password $postgrespass $ETCDENDPOINT
 
-$ETCDCTLCMD put /build/$prefix/postgres/user/dbname sil_db_$prefix
-$ETCDCTLCMD put /build/$prefix/postgres/user/username sil_user_$prefix
-$ETCDCTLCMD put /build/$prefix/postgres/user/password sil_password_$prefix
+$ETCDCTLCMD put /build/$prefix/postgres/user/dbname sil_db_$prefix $ETCDENDPOINT
+$ETCDCTLCMD put /build/$prefix/postgres/user/username sil_user_$prefix $ETCDENDPOINT
+$ETCDCTLCMD put /build/$prefix/postgres/user/password sil_password_$prefix $ETCDENDPOINT
 
 # set elastic env
-$ETCDCTLCMD put /build/$prefix/elastic/hostname $elastichost
-$ETCDCTLCMD put /build/$prefix/elastic/indexalias sil_$prefix
+$ETCDCTLCMD put /build/$prefix/elastic/hostname $elastichost $ETCDENDPOINT
+$ETCDCTLCMD put /build/$prefix/elastic/indexalias sil_$prefix $ETCDENDPOINT
 
 # set symfony env
-$ETCDCTLCMD put /build/$prefix/symfony/env test # maybe put this in env variable (or not)
-$ETCDCTLCMD put /build/$prefix/symfony/addr '127.0.0.1:8042'
+$ETCDCTLCMD put /build/$prefix/symfony/env test $ETCDENDPOINT # maybe put this in env variable (or not)
+$ETCDCTLCMD put /build/$prefix/symfony/addr '127.0.0.1:8042' $ETCDENDPOINT
 
 
 
-$ETCDCTLCMD get --endpoints="http://${ETCDHOST}:2379" --prefix /build/$prefix
+$ETCDCTLCMD get  --prefix /build/$prefix $ETCDENDPOINT
