eb1211cfcdd66e5e8d7cc37b966800bfb2a78bee
François GERBES <francois.gerbes@libre-informatique.fr>
2018-03-12 13:01:22 +0100

    [CI] update app/config/parameters.yml.dist only if app/config/parameters.yml does not exist

--- bin/ci-scripts/set_db_host_test.sh
+++ bin/ci-scripts/set_db_host_test.sh
@@ -19,20 +19,21 @@ fi
 #### POSTGRES ####
 
 
-# TODO
-# should remove db info from  app/config/config_test.yml
-# and use sed or etcd or confd or both to update parameters.yml.dist or create parameters.yml
+
 # sed -e s/'database_host: 127.0.0.1'/'database_host: ${DBHOST}'/g -i app/config/parameters.yml.dist
 if [ -n "${DBHOST}" ]
 then
-    # sed -e s/'127.0.0.1'/${DBHOST}/g -i app/config/config_test.yml
-    sed -e s/'database_host: 127.0.0.1'/"database_host: ${DBHOST}"/g -i app/config/parameters.yml.dist
-    #sed -e s/'database_port: 5432'/"database_port: 5432"/g -i app/config/parameters.yml.dist
-    sed -e s/'database_name: sil'/"database_name: ${DBAPPNAME}"/g -i app/config/parameters.yml.dist
-    sed -e s/'database_user: sil_user'/"database_user: ${DBAPPUSER}"/g -i app/config/parameters.yml.dist
-    sed -e s/'database_password: sil'/"database_password: ${DBAPPPASSWORD}"/g -i app/config/parameters.yml.dist
-    #TODO
-    # should use env var from etcd (for password)
+    # only if parameters have not been generated before (by etcd script for example)
+    if [ ! -f  app/config/parameters.yml ]
+       then
+           # sed -e s/'127.0.0.1'/${DBHOST}/g -i app/config/config_test.yml
+           sed -e s/'database_host: 127.0.0.1'/"database_host: ${DBHOST}"/g -i app/config/parameters.yml.dist
+           #sed -e s/'database_port: 5432'/"database_port: 5432"/g -i app/config/parameters.yml.dist
+           sed -e s/'database_name: sil'/"database_name: ${DBAPPNAME}"/g -i app/config/parameters.yml.dist
+           sed -e s/'database_user: sil_user'/"database_user: ${DBAPPUSER}"/g -i app/config/parameters.yml.dist
+           sed -e s/'database_password: sil'/"database_password: ${DBAPPPASSWORD}"/g -i app/config/parameters.yml.dist
+    fi
+
     echo  ${DBHOST}:5432:*:${DBROOTUSER}:${DBROOTPASSWORD} >> $HOME/.pgpass
     chmod 600  $HOME/.pgpass
     cat  $HOME/.pgpass
@@ -43,8 +44,12 @@ fi
 #### ELASTIC ####
 if [ -n "${ELHOST}" ]
 then
-    sed -e s/'elastic_search.hostname:  127.0.0.1'/"elastic_search.hostname:  ${ELHOST}"/g -i app/config/parameters.yml.dist
-    sed -e s/'blast_search.global_index_alias: sil'/"blast_search.global_index_alias: ${ELALIAS}"/g -i app/config/parameters.yml.dist
+    # only if parameters have not been generated before (by etcd script for example)
+    if [ ! -f  app/config/parameters.yml ]
+    then
+        sed -e s/'elastic_search.hostname:  127.0.0.1'/"elastic_search.hostname:  ${ELHOST}"/g -i app/config/parameters.yml.dist
+        sed -e s/'blast_search.global_index_alias: sil'/"blast_search.global_index_alias: ${ELALIAS}"/g -i app/config/parameters.yml.dist
+    fi
 fi
 
 # DEBUG
