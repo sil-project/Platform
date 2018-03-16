467c17fd6da15d1cfd28cfce76608e228980f00f
François GERBES <francois.gerbes@libre-informatique.fr>
2018-03-16 15:41:53 +0100

    [CI] Rename workspace
    https://github.com/sil-project/Platform/issues/53

--- src/Sil/Component/Contact/Model/Province.php
+++ src/Sil/Component/Contact/Model/Province.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
- * Copyright (C) 2015-2017 Libre Informatique
+ * Copyright (C) 2015-2018 Libre Informatique
  *
  * This file is licenced under the GNU LGPL v3.
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
-namespace Sil\Bundle\CRMBundle\Entity;
+namespace Sil\Component\Contact\Model;
 
 use Blast\Component\Resource\Model\ResourceInterface;
 
