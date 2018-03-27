a22e55bfe96d04532ef78361c0d787512afb0ac8
François GERBES <francois.gerbes@libre-informatique.fr>
2018-03-27 13:55:04 +0200

    [CI][DoctrineSessionBundle] Add a smart comment

--- src/Blast/Bundle/DoctrineSessionBundle/Entity/Session.php
+++ src/Blast/Bundle/DoctrineSessionBundle/Entity/Session.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2015-2017 Libre Informatique
+ * Copyright (C) 2015-2018 Libre Informatique
  *
  * This file is licenced under the GNU LGPL v3.
  * For the full copyright and license information, please view the LICENSE.md
@@ -10,6 +10,7 @@
 
 namespace Blast\Bundle\DoctrineSessionBundle\Entity;
 
+/*@todo: should we use Symfony\Component\HttpFoundation\SessionInterface ? */
 /**
  * Session.
  */
