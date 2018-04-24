45f607537f6c99f1469d8b27ed8990488e2c9324
François GERBES <francois.gerbes@libre-informatique.fr>
2018-04-24 11:00:15 +0200

    [CI] Use Branch Name from Jenkins

--- bin/ci-scripts/gen_archive.sh
+++ bin/ci-scripts/gen_archive.sh
@@ -5,7 +5,10 @@ Name=Platform
 # git symbolic-ref -q --short HEAD || git describe --tags --exact-match
 #Version=$(git describe --tags)
 #Tag=$(git describe --tags --abbrev=0)
-Branch=$(git name-rev  --name-only $(git rev-parse HEAD) | sed -e s/\\^.*//g | awk -F'/' '{print $(NF)}')
+if [ -z ${Branch} ]
+then
+    Branch=$(git name-rev  --name-only $(git rev-parse HEAD) | sed -e s/\\^.*//g | awk -F'/' '{print $(NF)}')
+fi
 
 # Clean current git dir
 git clean -df
